7440c8c333e14c4b687f3fcdf5cd0b7c55808ed6
sarahfeldmann <sarahf95@gmail.com>
2017-10-24 21:35:33 -0700

    Add doubles functionality

--- SimpleCalc-iOS/SimpleCalc-iOS/ViewController.swift
+++ SimpleCalc-iOS/SimpleCalc-iOS/ViewController.swift
@@ -24,9 +24,9 @@ class ViewController: UIViewController {
     // MARK: Actions
 
     var action = false
-    var num = 0
+    var num = 0.0
     var operation = ""
-    var nums = [Int]()
+    var nums = [Double]()
     
     // Numbers
     @IBAction func clickZero(_ sender: Any) {
@@ -98,76 +98,86 @@ class ViewController: UIViewController {
             result.text = result.text! + "9"
         }
     }
+    @IBAction func clickPoint(_ sender: Any) {
+        result.text! += "."
+    }
+    
     
     // Clear
     @IBAction func clickClear(_ sender: Any) {
         result.text = "0"
         num = 0
-        nums = [Int]()
+        nums = [Double]()
     }
     
     // Operations
     @IBAction func clickPlus(_ sender: Any) {
         action = true
-        num = (result.text! as NSString).integerValue
+        num = (result.text! as NSString).doubleValue
         operation = "add"
+        result.text = String(num)
     }
     @IBAction func clickSub(_ sender: Any) {
         action = true
-        num = (result.text! as NSString).integerValue
+        num = (result.text! as NSString).doubleValue
         operation = "sub"
+        result.text = String(num)
     }
     @IBAction func clickTimes(_ sender: Any) {
         action = true
-        num = (result.text! as NSString).integerValue
+        num = (result.text! as NSString).doubleValue
         operation = "mult"
+        result.text = String(num)
     }
     @IBAction func clickDiv(_ sender: Any) {
         action = true
-        num = (result.text! as NSString).integerValue
+        num = (result.text! as NSString).doubleValue
         operation = "div"
+        result.text = String(num)
     }
     @IBAction func clickMod(_ sender: Any) {
         action = true
-        num = (result.text! as NSString).integerValue
+        num = (result.text! as NSString).doubleValue
         operation = "mod"
+        result.text = String(num)
     }
     @IBAction func clickCount(_ sender: Any) {
         action = true
-        nums.append((result.text! as NSString).integerValue)
+        nums.append((result.text! as NSString).doubleValue)
         operation = "count"
+        result.text = String((result.text! as NSString).doubleValue)
     }
     
     @IBAction func clickAvg(_ sender: Any) {
         action = true
-        nums.append((result.text! as NSString).integerValue)
+        nums.append((result.text! as NSString).doubleValue)
         operation = "avg"
+        result.text = String((result.text! as NSString).doubleValue)
     }
     @IBAction func clickFact(_ sender: Any) {
         action = true
-        nums.append((result.text! as NSString).integerValue)
+        nums.append((result.text! as NSString).doubleValue)
         operation = "fact"
+        result.text = String((result.text! as NSString).doubleValue)
     }
-    // Equals
-   
     
+    // Equals
     @IBAction func clickEquals(_ sender: Any) {
         if action {
             switch operation{
                 case "add":
-                result.text = String(Int(num) + Int((result.text! as NSString).integerValue))
+                result.text = String(Double(num) + (result.text! as NSString).doubleValue)
                 action = false
             case "sub":
-                result.text = String(Int(num) -
-                    Int((result.text! as NSString).integerValue))
+                result.text = String(Double(num) - (result.text! as NSString).doubleValue)
                 action = false
             case "mult":
-                result.text = String(Int(num) * Int((result.text! as NSString).integerValue))
+                result.text = String(Double(num) * (result.text! as NSString).doubleValue)
             case "div":
-                result.text = String(Int(num) / Int((result.text! as NSString).integerValue))
+                result.text = String(Double(num) / (result.text! as NSString).doubleValue)
                 action = false
             case "mod":
-                result.text = String(Int(Double(num).truncatingRemainder(dividingBy: Double((result.text! as NSString).integerValue))))
+                result.text = String(Double(num).truncatingRemainder(dividingBy: (result.text! as NSString).doubleValue))
                     action = false
             case "count":
                 if(nums.count == 1){
@@ -175,25 +185,29 @@ class ViewController: UIViewController {
                 } else {
                     result.text = String(nums.count + 1)
                 }
-                nums = [Int]()
+                nums = [Double]()
+                action = false
             case "avg":
-                nums.append((result.text! as NSString).integerValue)
-                var total = 0
+                nums.append((result.text! as NSString).doubleValue)
+                var total = 0.0
                 for i in 0...nums.count-1 {
                     total += nums[i]
                 }
-                result.text = String(total/nums.count)
-                nums = [Int]()
+                result.text = String(total/Double(nums.count))
+                nums = [Double]()
+                action = false
             case "fact":
                 if nums.count > 1 || String(nums[0]) != result.text{
                     result.text = "ERROR"
                 } else {
                     var total = 1
-                    for i in 1...nums[0] {
+                    for i in 1...Int(nums[0] ){
                         total *= i
                     }
-                    result.text = String(total)
+                    result.text = String(Double(total))
                 }
+                nums = [Double]()
+                action = false
             default:
                 result.text = result.text
             }
